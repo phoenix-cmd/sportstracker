af22389eeb300c685cc7127ba885f5705c7eea97
phoenix-cmd <bs162728@gmail.com>
2025-03-08 00:15:35 +0530

    Revert "Fixed some bugs"
    This reverts commit e4da57e3602335ac1ec692516eb2f130536fd7c6.

--- sportstracker/src/App.jsx
+++ sportstracker/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:sportstracker/src/App.jsx
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -66,3 +67,64 @@ function App() {
 
 export default App;
 
+=======
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import Matches from "./pages/Matches";
+import Analytics from "./pages/Analytics";
+import PlayerDetails from "./pages/PlayerDetails";
+import Admin from "./pages/Admin";
+import Landing from "./pages/Landing";
+import Signup from "./pages/Signup";
+import CricketMatches from "./pages/CricketMatches";
+import FootballMatches from "./pages/FootballMatches";
+import AddPlayer from "./pages/AddPlayer";
+import TeamPage from "./pages/TeamPage";
+import BadmintonMatches from "./pages/BadmintonMatches";
+
+function App() {
+  const location = useLocation();
+  const hideNavbarFooter = [
+    "/",
+    "/signup",
+    "/landing",
+    "/admin/",
+    "/admin/teams",
+    "/admin/add-player",
+    "/admin/cricket-matches",
+    "/admin/",
+  ].includes(location.pathname); // ✅ Now hides on Landing Page too
+
+  return (
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col">
+      {!hideNavbarFooter && <Navbar />}{" "}
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />{" "}
+          {/* ✅ Route for explicit Landing Page */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/matches" element={<Matches />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/players/:playerName" element={<PlayerDetails />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin/cricket-matches" element={<CricketMatches />} />
+          <Route path="/admin/add-player" element={<AddPlayer />} />
+          <Route path="/admin/teams" element={<TeamPage />} />
+        </Routes>
+      </div>
+      {!hideNavbarFooter && <Footer />}
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> parent of e4da57e (Fixed some bugs):src/App.jsx
